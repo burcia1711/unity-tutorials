e0ff867bee728fe723450faaf0a8a650984fd19f
BURCU CINAR <burcu.alakus@gmail.com>
2022-12-29 22:38:52 +0300

    monster chase 1

---
+++ Monster Chase/Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveForce = 10f;
+
+    [SerializeField]
+    private float jumpForce = 11f;
+
+    private float movementX;
+    private Rigidbody2D myBody;
+    private Animator anim;
+    private string WALK_ANIMATION = "Walk";
+    private SpriteRenderer sr;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveKeyboard();
+        AnimatePlayer();
+    }
+
+    void PlayerMoveKeyboard()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+
+        transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+    }
+
+    void AnimatePlayer()
+    {
+       if(movementX > 0)
+        {
+            sr.flipX = false;
+            anim.SetBool(WALK_ANIMATION, true);
+        }
+       else if (movementX < 0)
+        {
+            sr.flipX = true;
+            anim.SetBool(WALK_ANIMATION, true);
+        }
+        else
+        {
+            anim.SetBool(WALK_ANIMATION, false);
+        }
+    }
+
+}
