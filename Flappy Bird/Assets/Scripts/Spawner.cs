f355a5b605de56e7142b431ea8a032a922dac652
BURCU CINAR <burcu.alakus@gmail.com>
2023-01-05 21:55:49 +0300

    my flappy bird v2 pipes spawner added

---
+++ Flappy Bird/Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Spawner : MonoBehaviour
+{
+    private GameObject player;
+    public GameObject prefab;
+    public float spawnRate = 1f;
+    public float minHeight = -5f;
+    public float maxHeight = -1f;
+
+    public Vector3 Position { get; private set; }
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void OnEnable()
+    {
+        InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke(nameof(Spawn));
+    }
+
+    private void Spawn()
+
+    {
+        Position = player.transform.position + new Vector3(20, 0, 0);
+        GameObject pipes = Instantiate(prefab, new Vector3(Position.x, 0, -10), Quaternion.identity);
+        pipes.transform.position += Vector3.up * Random.Range(minHeight, maxHeight);
+    }
+
+}
