eb336098cd85c17066b6ab69866bab67e3778109
BURCU CINAR <burcu.alakus@gmail.com>
2023-01-28 14:47:09 +0300

    minesweeper day 1

---
+++ Minesweeper/Assets/Scripts/Cell.cs
@@ -0,0 +1,20 @@
+
+using UnityEngine;
+
+public class Cell
+{
+    public enum Type
+    {
+        Empty,
+        Mine,
+        Number,
+    }
+
+    public Vector3Int position;
+    public Type type;
+    public int number;
+    public bool revealed;
+    public bool flagged;
+    public bool exploded;
+
+}
